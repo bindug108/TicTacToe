0b1a3e8d96c5c53a5484a85b4d3a438c7d2cebac
bindug108 <bindug@hotmail.co.uk>
2018-06-21 12:11:32 +0100

    Initial Commit

--- TicTacToe/Startup.cs
+++ TicTacToe/Startup.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using TicTacToe.Services;
+using TicTacToe.Extensions;
+using Microsoft.AspNetCore.Routing;
+using TicTacToe.Models;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace TicTacToe
 {
@@ -15,6 +20,10 @@ namespace TicTacToe
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            //services.AddDirectoryBrowser();
+            services.AddSingleton<IUserService, UserService>();
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -23,12 +32,55 @@ namespace TicTacToe
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
+            else
+                app.UseExceptionHandler("/Home/Error");
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseStaticFiles();
+
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("CreateUser", context =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var firstName = context.Request.Query["firstName"];
+                var lastName = context.Request.Query["lastName"];
+                var email = context.Request.Query["email"];
+                var password = context.Request.Query["password"];
+
+                var userService = context.RequestServices.GetService<IUserService>();
+                userService.RegisterUser(new UserModel
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    Password = password
+                });
+
+                return context.Response.WriteAsync($"User {firstName} {lastName} has been successfully created");
             });
+
+            var newUserRoutes = routeBuilder.Build();
+            app.UseRouter(newUserRoutes);
+
+            var options = new RewriteOptions();
+            options.AddRewrite("NewUser", "/UserRegistration/Index", false);
+            app.UseRewriter();
+
+            app.UseCommunicationMiddleware();
+            //app.UseDirectoryBrowser();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseStatusCodePages("text/plain", "HTTP Error - Status Code: {0}");
         }
     }
 }
